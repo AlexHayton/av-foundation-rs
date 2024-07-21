26e6c022d7e3b9c1b4a879fc7ca97ca29beefd21
Alex Hayton <alex.hayton@hopin.to>
2024-07-21 06:50:02 +0100

    iOS fixes

--- src/capture_device.rs
+++ src/capture_device.rs
@@ -3,9 +3,9 @@ use core_media::{
     format_description::{CMFormatDescription, CMFormatDescriptionRef},
     time::CMTime,
 };
-use objc2::{extern_class, msg_send, msg_send_id, mutability::InteriorMutable, rc::Id, ClassType};
+use objc2::{extern_class, msg_send, msg_send_id, mutability::InteriorMutable, rc::Id, ClassType, Encode, Encoding, RefEncode};
 use objc2_foundation::{
-    CGPoint, NSArray, NSError, NSInteger, NSObject, NSObjectProtocol, NSString,
+    CGFloat, CGPoint, NSArray, NSError, NSInteger, NSObject, NSObjectProtocol, NSString
 };
 
 use crate::{capture_session_preset::AVCaptureSessionPreset, media_format::AVMediaType};
@@ -16,6 +16,7 @@ extern "C" {
     pub static AVCaptureExposureTargetBiasCurrent: &'static f32;
     pub static AVCaptureExposureDurationCurrent: &'static CMTime;
     pub static AVCaptureISOCurrent: &'static f32;
+    pub static AVCaptureMaxAvailableTorchLevel: &'static f32;
 }
 
 extern "C" {
@@ -208,13 +209,16 @@ impl AVCaptureDevice {
         unsafe { msg_send![self, isExposureModeSupported: exposure_mode] }
     }
 
-    pub fn is_exposure_point_of_interest_supported(&self) -> bool {
-        unsafe { msg_send![self, isExposurePointOfInterestSupported] }
+    pub fn exposure_mode(&self) -> AVCaptureExposureMode {
+        unsafe { msg_send![self, exposureMode] }
     }
 
-    #[cfg(target_os = "ios")]
-    pub fn exposure_duration() -> CMTime {
-        unsafe { msg_send![self, exposureDuration] }
+    pub fn set_exposure_mode(&self, mode: AVCaptureExposureMode) -> AVCaptureExposureMode {
+        unsafe { msg_send![self, exposureMode: mode] }
+    }
+
+    pub fn is_exposure_point_of_interest_supported(&self) -> bool {
+        unsafe { msg_send![self, isExposurePointOfInterestSupported] }
     }
 
     pub fn exposure_point_of_interest(&self) -> CGPoint {
@@ -223,6 +227,53 @@ impl AVCaptureDevice {
 
     pub fn set_exposure_point_of_interest(&self, point: CGPoint) -> CGPoint {
         unsafe { msg_send![self, exposurePointOfInterest: point] }
+    }
+}
+
+#[cfg(target_os = "ios")]
+impl AVCaptureDevice {
+    pub fn exposure_duration(&self) -> CMTime {
+        unsafe { msg_send![self, exposureDuration] }
+    }
+
+    pub fn active_max_exposure_duration(&self) -> CMTime {
+        unsafe { msg_send![self, activeMaxExposureDuration] }
+    }
+
+    pub fn ISO(&self) -> f32 {
+        unsafe { msg_send![self, ISO] }
+    }
+
+    pub fn lens_aperture(&self) -> f32 {
+        unsafe { msg_send![self, lensAperture] }
+    }
+
+    pub fn is_face_drive_auto_expoure_enabled(&self) -> bool {
+        unsafe { msg_send![self, isFaceDrivenAutoExposureEnabled] }
+    }
+
+    pub fn automatically_adjusts_face_driven_auto_exposure_enabled(&self) -> bool {
+        unsafe { msg_send![self, automaticallyAdjustsFaceDrivenAutoExposureEnabled] }
+    }
+
+    pub fn set_automatically_adjusts_face_driven_auto_exposure_enabled(&self, value: bool) -> bool {
+        unsafe { msg_send![self, automaticallyAdjustsFaceDrivenAutoExposureEnabled: value] }
+    }
+
+    pub fn exposure_target_offset(&self) -> f32 {
+        unsafe { msg_send![self, exposureTargetOffset] }
+    }
+
+    pub fn exposure_target_bias(&self) -> f32 {
+        unsafe { msg_send![self, exposureTargetBias] }
+    }
+
+    pub fn minExposureTargetBias(&self) -> f32 {
+        unsafe { msg_send![self, minExposureTargetBias] }
+    }
+
+    pub fn maxExposureTargetBias(&self) -> f32 {
+        unsafe { msg_send![self, maxExposureTargetBias] }
     }
 }
 
@@ -243,6 +294,76 @@ impl AVCaptureDevice {
     pub fn set_focus_mode(&self, focus_mode: AVCaptureFocusMode) -> AVCaptureFocusMode {
         unsafe { msg_send![self, focusMode: focus_mode] }
     }
+
+    pub fn is_focus_point_of_interest_supported(&self) -> bool {
+        unsafe { msg_send![self, isFocusPointOfInterestSupported] }
+    }
+
+    pub fn focus_point_of_interest(&self) -> CGPoint {
+        unsafe { msg_send![self, focusPointOfInterest] }
+    }
+
+    pub fn set_focus_point_of_interest(&self, point: CGPoint) -> CGPoint {
+        unsafe { msg_send![self, focusPointOfInterest: point] }
+    }
+
+    pub fn minimum_focus_distance(&self) -> f32 {
+        unsafe { msg_send![self, minimumFocusDistance] }
+    }
+}
+
+#[cfg(target_os = "ios")]
+impl AVCaptureDevice {
+    pub fn is_smooth_auto_focus_supported(&self) -> bool {
+        unsafe { msg_send![self, isSmoothAutoFocusSupported] }
+    }
+
+    pub fn is_smooth_auto_focus_enabled(&self) -> bool {
+        unsafe { msg_send![self, isSmoothAutoFocusEnabled] }
+    }
+
+    pub fn is_face_driven_auto_focus_enabled(&self) -> bool {
+        unsafe { msg_send![self, isFaceDrivenAutoFocusEnabled] }
+    }
+
+    pub fn automatically_adjust_face_drive_auto_focus_enabled(&self) -> bool {
+        unsafe { msg_send![self, automaticallyAdjustsFaceDrivenAutoFocusEnabled] }
+    }
+
+    pub fn is_locking_focus_with_custom_lens_position_supported(&self) -> bool {
+        unsafe { msg_send![self, isLockingFocusWithCustomLensPositionSupported] }
+    }
+
+    pub fn lens_position(&self) -> f32 {
+        unsafe { msg_send![self, lensPosition] }
+    }
+
+    pub fn set_lens_position(&self, lens_position: f32) -> f32 {
+        unsafe { msg_send![self, lensPosition: lens_position] }
+    }
+}
+
+pub type AVCaptureAutoFocusRangeRestriction = NSInteger;
+pub const AVCaptureAutoFocusRangeRestrictionNone: AVCaptureAutoFocusRangeRestriction = 0;
+pub const AVCaptureAutoFocusRangeRestrictionNear: AVCaptureAutoFocusRangeRestriction = 1;
+pub const AVCaptureAutoFocusRangeRestrictionFar: AVCaptureAutoFocusRangeRestriction = 2;
+
+#[cfg(target_os = "ios")]
+impl AVCaptureDevice {
+    pub fn is_auto_focus_range_restriction_supported(&self) -> bool {
+        unsafe { msg_send![self, isAutoFocusRangeRestrictionSupported] }
+    }
+
+    pub fn auto_focus_range_restriction(&self) -> AVCaptureAutoFocusRangeRestriction {
+        unsafe { msg_send![self, autoFocusRangeRestriction] }
+    }
+
+    pub fn set_auto_focus_range_restriction(
+        &self,
+        range_restriction: AVCaptureAutoFocusRangeRestriction,
+    ) -> AVCaptureAutoFocusRangeRestriction {
+        unsafe { msg_send![self, autoFocusRangeRestriction: range_restriction] }
+    }
 }
 
 pub type AVCaptureWhiteBalanceMode = NSInteger;
@@ -259,6 +380,15 @@ pub struct AVCaptureWhiteBalanceGains {
     pub redGain: f32,
 }
 
+unsafe impl Encode for AVCaptureWhiteBalanceGains {
+    const ENCODING: Encoding =
+        Encoding::Struct("AVCaptureWhiteBalanceGains", &[CGFloat::ENCODING, CGFloat::ENCODING, CGFloat::ENCODING]);
+}
+
+unsafe impl RefEncode for AVCaptureWhiteBalanceGains {
+    const ENCODING_REF: Encoding = Encoding::Pointer(&Self::ENCODING);
+}
+
 impl AVCaptureDevice {
     pub fn is_white_balance_mode_supported(
         &self,
@@ -271,11 +401,99 @@ impl AVCaptureDevice {
         unsafe { msg_send![self, whiteBalanceMode] }
     }
 
+    pub fn is_adjusting_white_balance(&self) -> bool {
+        unsafe { msg_send![self, isAdjustingWhiteBalance] }
+    }
+
     pub fn set_white_balance_mode(
         &self,
         white_balance_mode: AVCaptureWhiteBalanceMode,
     ) -> AVCaptureWhiteBalanceMode {
         unsafe { msg_send![self, whiteBalanceMode: white_balance_mode] }
+    }
+}
+
+#[cfg(target_os = "ios")]
+impl AVCaptureDevice {
+    pub fn device_white_balance_gains(&self) -> AVCaptureWhiteBalanceGains {
+        unsafe { msg_send![self, deviceWhiteBalanceGains] }
+    }
+
+    pub fn gray_world_device_white_balance_gains(&self) -> AVCaptureWhiteBalanceGains {
+        unsafe { msg_send![self, grayWorldDeviceWhiteBalanceGains] }
+    }
+
+    pub fn max_white_balance_gains(&self) -> f32 {
+        unsafe { msg_send![self, maxWhiteBalanceGain] }
+    }
+}
+
+pub type AVCaptureFlashMode = NSInteger;
+pub const AVCaptureFlashModeOff: AVCaptureFlashMode = 0;
+pub const AVCaptureFlashModeOn: AVCaptureFlashMode = 1;
+pub const AVCaptureFlashModeAuto: AVCaptureFlashMode = 2;
+
+impl AVCaptureDevice {
+    pub fn has_flash(&self) -> bool {
+        unsafe { msg_send![self, hasFlash] }
+    }
+
+    pub fn is_flash_available(&self) -> bool {
+        unsafe { msg_send![self, isFlashAvailable] }
+    }
+}
+
+pub type AVCaptureTorchMode = NSInteger;
+pub const AVCaptureTorchModeOff: AVCaptureTorchMode = 0;
+pub const AVCaptureTorchModeOn: AVCaptureTorchMode = 1;
+pub const AVCaptureTorchModeAuto: AVCaptureTorchMode = 2;
+
+impl AVCaptureDevice {
+    pub fn has_torch(&self) -> bool {
+        unsafe { msg_send![self, hasTorch] }
+    }
+
+    pub fn is_torch_available(&self) -> bool {
+        unsafe { msg_send![self, isTorchAvailable] }
+    }
+
+    pub fn is_torch_active(&self) -> bool {
+        unsafe { msg_send![self, isTorchActive] }
+    }
+
+    pub fn is_torch_mode_supported(&self, torch_mode: AVCaptureTorchMode) -> bool {
+        unsafe { msg_send![self, isTorchModeSupported: torch_mode] }
+    }
+
+    pub fn torch_mode(&self) -> AVCaptureTorchMode {
+        unsafe { msg_send![self, torchMode] }
+    }
+
+    pub fn set_torch_mode_on_with_level(&self, level: f32) -> f32 {
+        unsafe { msg_send![self, setTorchModeOnWithLevel: level] }
+    }
+
+    pub fn set_torch_mode(&self, mode: AVCaptureTorchMode) -> AVCaptureTorchMode {
+        unsafe { msg_send![self, setTorchMode: mode] }
+    }
+}
+
+#[cfg(target_os = "ios")]
+impl AVCaptureDevice {
+    pub fn is_low_light_boost_supported(&self) -> bool {
+        unsafe { msg_send![self, isLowLightBoostSupported] }
+    }
+
+    pub fn is_low_light_boost_enabled(&self) -> bool {
+        unsafe { msg_send![self, isLowLightBoostEnabled] }
+    }
+
+    pub fn automatically_enables_low_light_boost_when_available(&self) -> bool {
+        unsafe { msg_send![self, automaticallyEnablesLowLightBoostWhenAvailable] }
+    }
+
+    pub fn set_automatically_enables_low_light_boost_when_available(&self, value: bool) -> bool {
+        unsafe { msg_send![self, automaticallyEnablesLowLightBoostWhenAvailable: value] }
     }
 }
 
@@ -295,25 +513,18 @@ impl AVCaptureDevice {
     }
 }
 
+#[cfg(target_os = "ios")]
 impl AVCaptureDevice {
-    #[cfg(target_os = "ios")]
     pub fn automatically_adjusts_video_hdr_enabled(&self) -> bool {
         unsafe { msg_send![self, automaticallyAdjustsVideoHDREnabled] }
     }
 
-    #[cfg(target_os = "ios")]
     pub fn is_video_hdr_enabled(&self) -> bool {
         unsafe { msg_send![self, isVideoHDREnabled] }
     }
 
-    #[cfg(target_os = "ios")]
-    pub fn automatically_adjusts_face_driven_auto_exposure_enabled(&self) -> bool {
-        unsafe { msg_send![self, automaticallyAdjustsFaceDrivenAutoExposureEnabled] }
-    }
-
-    #[cfg(target_os = "ios")]
-    pub fn set_automatically_adjusts_face_driven_auto_exposure_enabled(&self, value: bool) -> bool {
-        unsafe { msg_send![self, automaticallyAdjustsFaceDrivenAutoExposureEnabled: value] }
+    pub fn is_global_tone_mapping_enabled(&self) -> bool {
+        unsafe { msg_send![self, isGlobalToneMappingEnabled] }
     }
 }
 
@@ -327,6 +538,43 @@ pub const AVCaptureColorSpace_AppleLog: AVCaptureColorSpace = 3;
 impl AVCaptureDevice {
     pub fn active_color_space(&self) -> AVCaptureColorSpace {
         unsafe { msg_send![self, activeColorSpace] }
+    }
+}
+
+impl AVCaptureDevice {
+    pub fn display_zoom_factor_multiplier(&self) -> CGFloat {
+        unsafe { msg_send![self, displayZoomFactorMultiplier] }
+    }
+}
+
+#[cfg(target_os = "ios")]
+impl AVCaptureDevice {
+    pub fn video_zoom_factor(&self) -> f64 {
+        unsafe { msg_send![self, videoZoomFactor] }
+    }
+
+    pub fn set_video_zoom_factor(&self, factor: CGFloat) -> CGFloat {
+        unsafe { msg_send![self, videoZoomFactor: factor] }
+    }
+
+    pub fn is_ramping_video_zoom(&self) -> bool {
+        unsafe { msg_send![self, isRampingVideoZoom] }
+    }
+
+    pub fn min_available_video_zoom_factor(&self) -> CGFloat {
+        unsafe { msg_send![self, minAvailableVideoZoomFactor] }
+    }
+
+    pub fn max_available_video_zoom_factor(&self) -> CGFloat {
+        unsafe { msg_send![self, maxAvailableVideoZoomFactor] }
+    }
+
+    pub fn is_geometric_distortion_correction_supported(&self) -> bool {
+        unsafe { msg_send![self, isGeometricDistortionCorrectionSupported] }
+    }
+
+    pub fn geometric_distortion_correction_enabled(&self) -> bool {
+        unsafe { msg_send![self, geometricDistortionCorrectionEnabled] }
     }
 }
 
